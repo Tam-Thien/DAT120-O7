3a2c1c5d25b0a71b26e9f4204a80ac72c13bb857
Tam-Thien <taluo2800@uis.no>
2025-10-12 14:28:57 +0200

    Update Meny-valg.py

--- Meny-valg.py
+++ Meny-valg.py
@@ -21,46 +21,86 @@ def meny_liste():
 #meny_liste()
 
 def meny_valg(): 
-    try: 
-        valg = int(input("\nVelg tall fra meny: ")) #sikkrer at input tallet blir heltall så hele programmet ikke bare krasjer...  
+         
+    valg = int(input("\nVelg et tall fra menyen: ")) #sikkrer at input tallet blir heltall så hele programmet ikke bare krasjer...  
 
-        if valg == 1:
-            valg1()
-        elif valg == 2: 
-            valg2()
-        elif valg == 3:
-            valg3()
-        elif valg == 4:
-            valg4()
-        elif valg == 5:
-            valg5()
-        elif valg == 6:
-            valg6()
-        elif valg == 7:
-            valg7()
-        elif valg == 8:
-            valg8()
-        elif valg == 9:
-            valg9()
-        elif valg == 10:
-            valg10()
-        elif valg == 11:
-            valg11()
-        elif valg == 12:
-            valg12()
-        elif valg == 13:
-            valg13()
-        elif valg == 14: 
-            #valg14()
-            print("test14, i def_meny_valg for debug") #for debug, husk å rette opp koden før levering.
-        else:
-            print("Error...")
-    except ValueError:
-        print("ValueError :D")
+    if valg == 1:
+        valg1()
+    elif valg == 2: 
+        valg2()
+    elif valg == 3:
+        valg3()
+    elif valg == 4:
+        valg4()
+    elif valg == 5:
+        valg5()
+    elif valg == 6:
+        valg6()
+    elif valg == 7:
+        valg7()
+    elif valg == 8:
+        valg8()
+    elif valg == 9:
+        valg9()
+    elif valg == 10:
+        valg10()
+    elif valg == 11:
+        valg11()
+    elif valg == 12:
+        valg12()
+    elif valg == 13:
+        valg13()
+    elif valg == 14: 
+        #valg14()
+        print("test14, i def_meny_valg for debug") #for debug, husk å rette opp koden før levering.
+    else:
+        print("Error, please choose values from 1-14.")
+
+
+def valg1():
+    print("\ntest print for valg 1")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #EDIT main loop her etterpå, dette er hvertfall starten.
-meny_liste()
-meny_valg()
+meny_liste() #skriver ut meny valgene
+meny_valg()   #funksjon for hva som skjer når velger tallet. 
+              #Når du f.eks velger 1 går du inn i valg1() funksjonen som er å lage et nytt emne.
     
 
 
