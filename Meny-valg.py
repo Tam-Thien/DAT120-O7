70cc72e8b285c34fe8c085019c116453059ce4c6
Tam-Thien <taluo2800@uis.no>
2025-10-14 00:31:36 +0200

    Cleanup redesign v1.4
    Fjernet 90% av gammel kode og kommentarer derifra. Beholdt kun kjerne kode så vi har det mer ryddig og vi kan eventuelt expande senere ved behov.

--- Meny-valg.py
+++ Meny-valg.py
@@ -1,9 +1,7 @@
-#husk å legge til exceptions for value error og type error.
 
-#emne_liste = [] #ble tuklete å sette emne som argument på de andre bare for å få emne_liste inn i main loop.
+emner = {} # Istedenfor eempty list bruker vi empty dictionary. 
 
-#Printer ut menyen. 
-#Midlertidig kommentert ut meny 9-14 for oversiktlighet. 
+#Printer ut menyen. #Midlertidig kommentert ut meny 9-14 for oversiktlighet. 
 def meny_liste():
     print("\n________Meny:_______")   
     print("01. Lag et nytt emne.")
@@ -14,39 +12,8 @@ def meny_liste():
     print("06. Lagre emnene og studieplanen til fil")
     print("07. Les inn emnene og studieplanen fra fil.")
     print("08. Avslutt.")
-    
-    '''print("09. Frivillig: Slett et emne.")
-    print("10. Frivillig: Fjern et emne fra studieprogrammet uten å slette det fra emnelista.")
-    print("11. Frivillig: Legg til anbefalt valgemne.")
-    print("12. Frivillig: Fjern anbefalt valgemne.")
-    print("13. Frivillig: Legg til annet valgemne.")
-    print("14. Frivillig: Fjern annet valgemne.")'''
-
-
-# valg1() v1.3. 
-'''
-def valg1(): #1) Lag et nytt emne.
-    
-    emnekode = input("Skriv inn emnekode: ")
-    navn = input("Skriv inn navn på emnet: ")
-    sesong_input = int(input("Skriv inn 1 for høst eller 2 for vår: ")) #vi har 1 og 2 så vi slipper å tenke på lower case, space osv når folk skriver inn høst og vår. Kan eventuelt bruke h/v.
-    if sesong_input == 1: #grunnen til at det står sesong og ikke semester som i høst/vår semester er for å unngå overlapp i navn og forvirring siden i oppgave 2 trenger man å bruke "semester" i koden.
-        sesong = "høst"
-    elif sesong_input == 2:
-        sesong = "vår"
-    else:
-        print("vennligst velg 1 (for høst) eller 2 (for vår)") # Lag en try except blokk senere..............    
-    studiepoeng = int(input("Skriv inn studie poeng: "))
-    
-    
-    emne = {"emnekode": emnekode, "navn": navn, "sesong": sesong, "studiepoeng": studiepoeng} 
-    
-    emne_liste.append(emne)
-    print(f"Emne kode {emnekode} er lagt til.")
-'''
-
-emner = {} # Istedenfor eempty list bruker vi empty dictionary. 
-           # Dette erstatter tidligere kode som het emne_liste
+ 
+# valg1() v1.3. # Dette erstatter tidligere kode som het emne_liste
 def valg1(): 
 
     emnekode = input("Skriv inn emnekode: ")
@@ -68,48 +35,20 @@ def valg1():
         "sesong": sesong,
         "studiepoeng": studiepoeng
     }
-    
-
-#-------------------------- OK, tror jeg forstår feilen nå. når vi endrer på valg()1 så gjør endringen error i valg3() pga.  f formatering av emnekode.
+    print(f"Emnekode: {emnekode} er lagt til.")    
+#--- OK, tror jeg forstår feilen nå. når vi endrer på valg()1 så gjør endringen error i valg3() pga.  f formatering av emnekode.
 #skal prøve å fikse det.
-
+#Fiksa :D
 
 def valg3(): #Skriv ut liste over emner
-    '''for kode, data in emner.items():
-        print(f"{kode}: {data['navn']} {data['sesong']}, {data['studiepoeng']} studiepoeng")
-        #jeg vet det ser stygt ut, men den skal være sånn pga. indentation...'''
-    print("Emner:") # Denne ligger før "for løkken" siden vi bare ønsker å printe ut overskriften en gang.
+    print("\nEmner:") # Denne ligger før "for løkken" siden vi bare ønsker å printe ut overskriften en gang.
     for kode, data in emner.items(): # siden dictionary går par-vis som nøkkel og verdi. 
-        print(f"Emnekode: {kode}, Navn: {data['navn']}, Sesong: {data['sesong']}, Studiepoeng: {data['studiepoeng']} ") #kode er emnekode, data er verdier i emner.
-        #tilsvarende kode på generisk form blir vel nøkkel og verdi som dictionaries har. 
-
-
-    '''print("Emner:")
-    for nøkkel, verdi in emner.items(): # siden dictionary går par-vis som nøkkel og verdi.
-       
-       print(f"Emnekode:{nøkkel} {verdi['navn']} {verdi['sesong']}, {verdi['studiepoeng']}")
-    '''
-    
-    '''print("Emne liste:")
-    for emnekode in emner:
-        print(emne_liste) # tester men gammel søppel print uten struktur for debug.
-        
-        
-        #print(f"Emnekode: {emne["emnekode"]}, Navn: {emne["navn"]}, Sesong: {emne["sesong"]}, Studiepoeng: {emne["studiepoeng"]}")
-        # Vi vet at emne = {"emnekode": emnekode, "navn": navn, "sesong": sesong, "studiepoeng": studiepoeng}
-        # f formaterer så vi kan bruke variabler.
-        # {emne} henter variabler mens {emne['navn']} henter navn i emne.
-        # emne er en "ordbok" --> a dictionary. Basically liste.
-        # Begrunnelse for valg av dictionary istedenfor liste er fra anbefaling fra oppgaven. Det gjør det enklere å organisere,
-        #f.eks når man skal slette. Ulempen er at det kan ta lengre tid å kode, og man må slå opp "katalog" for å finne kode på emnet.
-'''
+        print(f"Emnekode: {kode}, Navn: {data['navn']}, Sesong: {data['sesong']}, Studiepoeng: {data['studiepoeng']} ") 
+        #kode er emnekode, data er verdier i emner. #tilsvarende kode på generisk form blir vell nøkkel og verdi som dictionaries har. 
 
 
 
- 
-
-#EDIT main loop her etterpå, dette er hvertfall starten.
-def hoved_program():
+def hoved_program(): #EDIT main loop her etterpå, dette er hvertfall starten.
     
     while True:
         meny_liste() #skriver ut meny valgene 
@@ -134,24 +73,9 @@ def hoved_program():
             print("Avslutter programmet")
             break
         else:
-            print("Feil, velg et tall mellom 1 og 8.") #husk å endre fra 8 til 14 når det implimenteres.
-'''        
-        elif valg == 9:
-            valg9()
-        elif valg == 10:
-            valg10()
-        elif valg == 11:
-            valg11()
-        elif valg == 12:
-            valg12()
-        elif valg == 13:
-            valg13()
-        elif valg == 14: 
-            #valg14()
-            print("test14, i def_meny_valg for debug") #for debug, husk å rette opp koden før levering. 
-'''
-        
-           
-                      
+            print("Feil, velg et tall mellom 1 og 8.") #husk å endre fra 8 til 14 når det implimenteres. 
+            #Slettet de andre, siden koden crashet da jeg kommenterte ut de andre.
+
+
 #Kjører hoved programmet for menyen
 hoved_program()
